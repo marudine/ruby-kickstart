c47a363a6608f62a3e19293ae3417f9a2695403e
Nicholas Leacock <nicholas@swissmail.com>
2017-01-23 15:33:37 +0000

    1:7

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,15 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  arr = []
+  string.chars.each_with_index do |r, i|
+    if r == "r" || r == "R"
+      arr << string[i + 1]
+    end
+  end
+  arr.compact.join
 end
