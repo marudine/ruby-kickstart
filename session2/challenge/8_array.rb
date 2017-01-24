58aa811e0bc7f0ce9f8ed78a6c026be1a07ec453
Nicholas Leacock <nicholas@swissmail.com>
2017-01-24 11:45:51 +0000

    2:8

--- session2/challenge/8_array.rb
+++ session2/challenge/8_array.rb
@@ -1,7 +1,16 @@
 # Given an array of elements, return true if any element shows up three times in a row
-# 
+#
 # Examples:
 # got_three? [1, 2, 2, 2, 3]  # => true
 # got_three? ['a', 'a', 'b']  # => false
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
+
+def got_three?(thrice)
+  thrice.each_with_index do |elm, i|
+    if elm == thrice[i + 1]
+      return true if elm == thrice[i + 2]
+    end
+  end
+  false
+end
